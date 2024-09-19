7c87b2ad0683177db5cf948aef8da3710435e2c8
Yavor Dashev <58559918+y-dashev@users.noreply.github.com>
2024-09-19 12:39:49 +0300

    A base to work on

--- lib/mix_to_calendar.ex
+++ lib/mix_to_calendar.ex
@@ -1,18 +1,21 @@
 defmodule MixToCalendar do
+  alias MixToCalendar.URLs
+  alias MixToCalendar.Sample
+
   @moduledoc """
   Documentation for `MixToCalendar`.
   """
 
   @doc """
-  Hello world.
+  Google URL
 
   ## Examples
 
-      iex> MixToCalendar.hello()
-      :world
+      iex> MixToCalendar.google_url()
+      Google Calendar URL: https://www.google.com/calendar/render?action=TEMPLATE&ctz=Europe/London&dates=2024-09-19T1000/2024-09-19T1100&details=Join%20us%20for%20an%20amazing%20conference%20on%20Elixir%20programming!%0A%0Ahttps://www.example.com&location=123%20Elixir%20St,%20London&text=Elixir%20Conference
 
   """
-  def hello do
-    :world
+  def google_url do
+    MixToCalendar.Sample.generate_google_url()
   end
 end
