810b68d985351517d3a82397d6a43dbd7a86316f
Yavor Dashev <58559918+y-dashev@users.noreply.github.com>
2024-09-19 12:51:53 +0300

    add proper test

--- lib/mix_to_calendar.ex
+++ lib/mix_to_calendar.ex
@@ -11,7 +11,6 @@ defmodule MixToCalendar do
   ## Examples
 
       iex> MixToCalendar.google_url()
-        Google Calendar URL: https://www.google.com/calendar/render?action=TEMPLATE&ctz=Europe/London&dates=2024-09-19T1000/2024-09-19T1100&details=Join%20us%20for%20an%20amazing%20conference%20on%20Elixir%20programming!%0A%0Ahttps://www.example.com&location=123%20Elixir%20St,%20London&text=Elixir%20Conference
 
   """
   def google_url do
--- test/mix_to_calendar_test.exs
+++ test/mix_to_calendar_test.exs
@@ -1,8 +1,45 @@
 defmodule MixToCalendarTest do
   use ExUnit.Case
   doctest MixToCalendar
+  alias MixToCalendar.URLs
 
-  test "greets the world" do
-    assert MixToCalendar.hello() == :world
+  describe "google_url/1" do
+    test "generates the correct Google Calendar URL" do
+      calendar_event = %URLs{
+        start_datetime: ~N[2024-09-19 10:00:00],
+        end_datetime: ~N[2024-09-19 11:00:00],
+        title: "Elixir Conference",
+        timezone: "Europe/London",
+        location: "123 Elixir St, London",
+        description: "Join us for an amazing conference on Elixir programming!",
+        url: "https://www.example.com",
+        add_url_to_description: true,
+        all_day: false
+      }
+
+      expected_url = "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Elixir+Conference&dates=20240919T100000/20240919T110000&details=Join+us+for+an+amazing+conference+on+Elixir+programming%21+https%3A%2F%2Fwww.example.com&location=123+Elixir+St%2C+London&ctz=Europe%2FLondon"
+
+      assert URLs.google_url(calendar_event) == expected_url
+    end
+  end
+
+  describe "outlook_url/1" do
+    test "generates the correct Outlook Calendar URL" do
+      calendar_event = %URLs{
+        start_datetime: ~N[2024-09-19 10:00:00],
+        end_datetime: ~N[2024-09-19 11:00:00],
+        title: "Elixir Conference",
+        timezone: "Europe/London",
+        location: "123 Elixir St, London",
+        description: "Join us for an amazing conference on Elixir programming!",
+        url: "https://www.example.com",
+        add_url_to_description: true,
+        all_day: false
+      }
+
+      expected_url = "https://outlook.live.com/calendar/0/deeplink/compose?path=/calendar/action/compose&subject=Elixir+Conference&startdt=2024-09-19T10:00:00&enddt=2024-09-19T11:00:00&body=Join+us+for+an+amazing+conference+on+Elixir+programming%21+https%3A%2F%2Fwww.example.com&location=123+Elixir+St%2C+London"
+
+      assert URLs.outlook_com_url(calendar_event) == expected_url
+    end
   end
 end
