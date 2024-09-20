a4fb7f2e640d7a45f737622232b97004505708ac
Yavor Dashev <58559918+y-dashev@users.noreply.github.com>
2024-09-20 10:10:56 +0300

    publish

--- mix.exs
+++ mix.exs
@@ -4,10 +4,13 @@ defmodule MixToCalendar.MixProject do
   def project do
     [
       app: :mix_to_calendar,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: "https://github.com/y-dashev/mix_to_calendar",
+      description: description(),
+      package: package()
     ]
   end
 
@@ -18,11 +21,23 @@ defmodule MixToCalendar.MixProject do
     ]
   end
 
+  defp description() do
+    "Generating URLs for calendars with Elixir."
+  end
+
+  defp package() do
+    [
+      files: ~w(lib  .formatter.exs mix.exs README* license*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/y-dashev/mix_to_calendar"}
+    ]
+  end
+
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
